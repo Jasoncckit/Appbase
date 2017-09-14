7acf8d8f9ce2568e688efae57d53c684d5bd5dc4
Jason Cheng <Jasoncckit@gmail.com>
2017-09-14 17:20:56 +1000

    Merged routes

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: '/about/',
+    component: require('./pages/about.vue')
+  },
+  {
+    path: '/form/',
+    component: require('./pages/form.vue')
+  },
+  {
+    path: '/dynamic-route/blog/:blogId/post/:postId/',
+    component: require('./pages/dynamic-route.vue')
+  },
+  {
+    path: '/hello/',
+    component: require('./components/Hello.vue')
+  }
+]
