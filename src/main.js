8db5f2107fbe63adf3c931b030a4f47e49d80247
Jason Cheng <Jasoncckit@gmail.com>
2017-09-14 17:23:02 +1000

    App integration

--- src/main.js
+++ src/main.js
@@ -1,15 +1,50 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+/* eslint-disable no-new */
+/* eslint-disable no-unused-vars */
 
+// Import Vue
+import Vue from 'vue'
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+// Import F7
+import Framework7 from 'framework7'
+
+// Import F7 Vue Plugin
+import Framework7Vue from 'framework7-vue'
+
+// Import F7 iOS Theme Styles
+// import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
+// import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
+// OR for Material Theme:
+import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
+import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
+
+// Import App Custom Styles
+// import AppStyles from './css/app.css'
+
+// Import Routes
+import Routes from './routes.js'
+
+// Import App Component
+import App from './App'
+
+// Init F7 Vue Plugin
+Vue.use(Framework7Vue)
+
+// Init App
 new Vue({
   el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+  template: '<app/>',
+  // Init Framework7 by passing parameters here
+  framework7: {
+    root: '#app',
+    /* Uncomment to enable Material theme: */
+    material: true,
+    routes: Routes
+  },
+  // Register App Component
+  components: {
+    app: App
+  }
 })
