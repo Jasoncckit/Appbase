500e68cadcc60550e7b7c88c6dd999d88e17c482
Jason Cheng <Jasoncckit@gmail.com>
2017-09-18 13:42:55 +1000

    Fixed linting

--- src/main.js
+++ src/main.js
@@ -3,16 +3,12 @@
 /* eslint-disable no-new */
 /* eslint-disable no-unused-vars */
 
-// Import Vue
 import Vue from 'vue'
-Vue.config.productionTip = false
-import axios from 'axios'
-
-// Import F7
 import Framework7 from 'framework7'
-
-// Import F7 Vue Plugin
 import Framework7Vue from 'framework7-vue'
+import axios from 'axios'
+import Routes from './routes.js'
+import App from './App'
 
 // Import F7 iOS Theme Styles
 // import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
@@ -21,29 +17,21 @@ import Framework7Vue from 'framework7-vue'
 import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
 import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
 
-// Import App Custom Styles
-// import AppStyles from './css/app.css'
+import AppStyles from './css/app.css'
 
-// Import Routes
-import Routes from './routes.js'
-
-// Import App Component
-import App from './App'
-
-// Init F7 Vue Plugin
+Vue.config.productionTip = false
 Vue.use(Framework7Vue)
 
-// Init App
 new Vue({
   el: '#app',
   template: '<app/>',
   // Init Framework7 by passing parameters here
   framework7: {
     root: '#app',
-    /* Uncomment to enable Material theme: */
+    routes: Routes,
+    /* Comment this to disable Material theme: */
     material: true,
 
-    routes: Routes,
     // Any other parameters, e.g.
     animateNavBackIcon: true,
     swipePanel: 'left',
