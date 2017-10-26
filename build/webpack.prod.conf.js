eda0c6ecce31dcb01403c2c808774f6a9d7e739d
Jason Cheng <Jasoncckit@gmail.com>
2017-10-26 15:37:31 +1100

    Leverage browser caching of all static assets

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -106,7 +106,13 @@ var webpackConfig = merge(baseWebpackConfig, {
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'dist/',
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/*/,
+          handler: 'cacheFirst'
+        }
+      ]
     })
   ]
 })
