4178dfb7e371e280721597b80b72fd260c4c6158
Jason Cheng <Jasoncckit@gmail.com>
2017-12-11 11:15:48 +1100

    Added runtime cache pattern

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -108,7 +108,13 @@ const webpackConfig = merge(baseWebpackConfig, {
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'dist/',
+      runtimeCaching: [
+        {
+          urlPattern: /(.*).(js|css|png|jpe\?g|gif|svg|mp4|webm|ogg|mp3|wav|flac|aac|woff2\?|eot|ttf|otf)/,
+          handler: 'cacheFirst'
+        }
+      ]
     })
   ]
 })
