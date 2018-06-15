13d1e0698f4ec1543029273c709829db521ee30e
Jason Cheng <Jasoncck@msn.com>
2018-06-15 10:19:13 +1000

    fixed Framework7 v2 build problem

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,5 +1,8 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
 module.exports = {
-  plugins: {
-    autoprefixer: {}
+  "plugins": {
+    // to edit target browsers: use "browserlist" field in package.json
+    "autoprefixer": {}
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,40 @@
-import '@babel/polyfill'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable no-new */
+
 import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Framework7 from 'framework7/dist/framework7.esm.bundle.js'
+import Framework7Vue from 'framework7-vue/dist/framework7-vue.esm.bundle.js'
+import axios from 'axios'
+import Routes from './routes.js'
+import App from './App'
+import Framework7Styles from 'framework7/dist/css/framework7.css'
+import AppStyles from './css/app.less'
 
 Vue.config.productionTip = false
+Vue.use(Framework7Vue, Framework7)
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  el: '#app',
+  template: '<app/>',
+  data () {
+    return {
+      Framework7Styles: Framework7Styles,
+      AppStyles: AppStyles,
+      axios: axios
+    }
+  },
+  // Init Framework7 by passing parameters here
+  framework7: {
+    root: '#app',
+    routes: Routes,
+    // Any other parameters, e.g.
+    animateNavBackIcon: true,
+    swipePanel: 'left',
+    hideNavbarOnPageScroll: true
+  },
+  // Register App Component
+  components: {
+    app: App
+  }
+})
