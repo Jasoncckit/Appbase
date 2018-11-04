fa794061afb2e7d48a6eb4d453580d944419d45a
Jason Cheng <Jasoncckit@gmail.com>
2018-11-05 10:38:30 +1100

    Modified landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <f7-nav-left>
         <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="left"></f7-link>
       </f7-nav-left>
-      <f7-nav-title>My App</f7-nav-title>
+      <f7-nav-title>Appbase</f7-nav-title>
       <f7-nav-right>
         <f7-link icon-if-ios="f7:menu" icon-if-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right>
