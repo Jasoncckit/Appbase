22a9abdcea9a57873dc40cd075330090ee7f48c7
Jason Cheng <Jasoncckit@gmail.com>
2018-11-05 11:34:38 +1100

    Imported dependencies

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
+import Framework7 from 'framework7/framework7.esm.bundle.js'
+import Framework7Vue from 'framework7-vue/framework7-vue.esm.bundle.js'
+import 'framework7/css/framework7.min.css'
+import 'framework7-icons/css/framework7-icons.css'
 import App from './App.vue'
-import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+Framework7.use(Framework7Vue)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount('#app')
