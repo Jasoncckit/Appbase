9980f705e0abc67ae2737042ee7a9d334eb21211
Jason Cheng <Jasoncckit@gmail.com>
2019-01-25 17:18:43 +1100

    Replaced baseUrl with publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,13 @@ module.exports = {
   pwa: {
     name: 'Appbase',
     themeColor: '#FAFAFA'
-  }
+  },
+
+  publicPath: './',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
